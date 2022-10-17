01e9342876548eb8fd2e21a93cc606709f0b6d8d
79650 <last137954682@gmail.ru>
2022-10-18 01:12:04 +0300

    task number 2

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8;
-        System.out.println(dog);
-        var cat = 3.6;
-        System.out.println(cat);
-        var paper = 763789;
-        System.out.println(paper);
-        dog = dog +4;
-        System.out.println(dog);
-        cat =cat + 4;
-        System.out.println(cat);
-        paper = paper + 4;
-        System.out.println(paper);
-        dog = dog - 3;
-        System.out.println(dog);
-        cat =cat - 1.6;
-        System.out.println(cat);
-        paper = paper - 7639;
-        System.out.println(paper);
+        System.out.println("Задание 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend +=2;
+        System.out.println(friend);
+        friend /=7;
+        System.out.println(friend);
+
+        System.out.println("Задание 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog *=10;
+        System.out.println(frog);
+        frog /=3.5;
+        System.out.println(frog);
+        frog +=4;
+        System.out.println(frog);
     }
 }
