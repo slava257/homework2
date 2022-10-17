4a9d869ca0f837c9248148b5f6bca2137a6d7ec7
79650 <last137954682@gmail.ru>
2022-10-18 00:35:01 +0300

    task number 1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog +4;
+        System.out.println(dog);
+        cat =cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        dog = dog - 3;
+        System.out.println(dog);
+        cat =cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+    }
+}
