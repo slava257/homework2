5e924e559c9a91535aae5895fad1dcbfea10d50a
79650 <last137954682@gmail.ru>
2022-10-18 13:16:33 +0300

    task number 3

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задание 4");
-        var friend = 19;
-        System.out.println(friend);
-        friend +=2;
-        System.out.println(friend);
-        friend /=7;
-        System.out.println(friend);
-
-        System.out.println("Задание 5");
-        var frog = 3.5;
-        System.out.println(frog);
-        frog *=10;
-        System.out.println(frog);
-        frog /=3.5;
-        System.out.println(frog);
-        frog +=4;
-        System.out.println(frog);
+        System.out.println("задание 6");
+        var weightOfOneBoxer =78.2;
+        var weightOfTheSecondBoxer =82.7;
+        var totalWeightOfBoxers = weightOfOneBoxer + weightOfTheSecondBoxer;
+        System.out.println("общий вес боксеров" + totalWeightOfBoxers +  "кг");
+        System.out.println("задание 7");
+        var boxerWeightDifference = weightOfTheSecondBoxer - weightOfOneBoxer;
+        System.out.println("разница в весе" + boxerWeightDifference +"кг!");
+        System.out.println("разница в весе" + (weightOfTheSecondBoxer % weightOfOneBoxer));
+        System.out.println("задание8");
+        var openingHours = 640;
+        var WorkingShift = 8;
+        var companyEmployees = openingHours / WorkingShift;
+        System.out.println("всего работников в компании" + companyEmployees+ "человек" );
+        var companyEmployees2 = 94;
+        var totalNumberOfEmployees = companyEmployees + companyEmployees2;
+        var totalWorkingHours = totalNumberOfEmployees * WorkingShift;
+        System.out.println("если в компании работает" + totalNumberOfEmployees + "человек, то всего" + totalWorkingHours +"часов работы может быть поделено между сотрудниками");
+        System.out.println("если в компанииработает" + (companyEmployees +  companyEmployees2) + "человек, то всего" +totalNumberOfEmployees * WorkingShift + "часов работы может быть поделено между сотрудниками");
     }
 }
